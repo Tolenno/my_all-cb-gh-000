23d2dd27d374ade6b63ec77e7b9bfba3c5983455
Matt Hough <matt@hough.tv>
2017-09-26 11:33:09 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,15 @@
 require 'pry'
 
 def my_all?(collection)
-
+  i = 0
+  while i < collection.size
+    yield(collection[i])
+    i += 1
+  end
 end
+
+1. initiate variable i to use as counter
+2. while i < collection.size
+3. yield collection to block
+4. index += 1
+5. return true/false
